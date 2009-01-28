b5f51dbf8a326575ed22579fae2a6ca5eb119ac8
Adenilson Cavalcanti <cavalcantii@gmail.com>
2009-01-28 14:16:22 -0400

    Aesthetics.

--- googledataresource.h
+++ googledataresource.h
@@ -9,28 +9,28 @@ extern "C" {
 class googledataResource : public Akonadi::ResourceBase,
                            public Akonadi::AgentBase::Observer
 {
-  Q_OBJECT
+	Q_OBJECT
 
-  public:
-    googledataResource( const QString &id );
-    ~googledataResource();
+	public:
+	googledataResource( const QString &id );
+	~googledataResource();
 
-  public Q_SLOTS:
-    virtual void configure( WId windowId );
+			     public Q_SLOTS:
+	virtual void configure( WId windowId );
 
-  protected Q_SLOTS:
-    void retrieveCollections();
-    void retrieveItems( const Akonadi::Collection &col );
-    bool retrieveItem( const Akonadi::Item &item, const QSet<QByteArray> &parts );
+					      protected Q_SLOTS:
+	void retrieveCollections();
+	void retrieveItems( const Akonadi::Collection &col );
+	bool retrieveItem( const Akonadi::Item &item, const QSet<QByteArray> &parts );
 
-  protected:
-    virtual void aboutToQuit();
+protected:
+	virtual void aboutToQuit();
 
-    virtual void itemAdded( const Akonadi::Item &item, const Akonadi::Collection &collection );
-    virtual void itemChanged( const Akonadi::Item &item, const QSet<QByteArray> &parts );
-    virtual void itemRemoved( const Akonadi::Item &item );
+	virtual void itemAdded( const Akonadi::Item &item, const Akonadi::Collection &collection );
+	virtual void itemChanged( const Akonadi::Item &item, const QSet<QByteArray> &parts );
+	virtual void itemRemoved( const Akonadi::Item &item );
 
-    gcal_t gcal;
+	gcal_t gcal;
 };
 
 #endif
