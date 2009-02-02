8dd899e7dfab9317535659dbf7906a882f87c3e7
Adenilson Cavalcanti <cavalcantii@gmail.com>
2009-02-02 08:29:00 -0400

    Adding a dialog to get user account + password.

---
+++ dlgGoogleDataConf.cpp
@@ -0,0 +1,10 @@
+#include <QtGui>
+#include "dlgGoogleDataConf.h"
+
+dlgGoogleDataConf::dlgGoogleDataConf(QWidget *parent): QDialog(parent)
+{
+	setupUi(this);
+
+	/* TODO: connect signals here */
+
+}
---
+++ dlgGoogleDataConf.h
@@ -0,0 +1,20 @@
+#ifndef __DLG_GOOGLE_DATA__
+#define __DLG_GOOGLE_DATA__
+
+#include <QDialog>
+#include "ui_GoogleData.h"
+
+class dlgGoogleDataConf : public QDialog, public Ui::dlgGoogleDataConf
+{
+
+Q_OBJECT
+public:
+	dlgGoogleDataConf(QWidget *parent = 0);
+
+private slots:
+
+	/* add some slots here */
+
+};
+
+#endif
